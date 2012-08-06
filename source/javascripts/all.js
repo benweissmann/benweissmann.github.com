cad0c37296cf23941fa74f03bf78945438626cf3
Ben Weissmann <benweissmann@gmail.com>
2012-08-05 22:07:16 -0700

    Finish projects page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,9 @@
 //= require jquery-1.7.2.min.js
 //= require bootstrap.min.js
 //= require_tree .
+
+$(function() {
+    $('.link-area').click(function(evt) {
+        window.location = $(evt.target).find('a.more').attr('href');
+    }).css('cursor', 'pointer')
+})
